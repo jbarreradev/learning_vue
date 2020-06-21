103af16dc64c1ae72fa3077a7581bd864f003219
Jhordy Barrera <jbarreraballestas@gmail.com>
2020-06-21 16:19:11 -0500

    v

--- js.js
+++ js.js
@@ -4,20 +4,33 @@ var app = new Vue({
       message: 'Hello Vue!',
       items: [
           {name: 'Mango', value: 1000, quantity: 10},
-          {name: 'Patilla', value: 5500, quantity: 10},
+          {name: 'Patilla', value: 5500, quantity: 31},
           {name: 'Pera', value: 2000, quantity: 0},
-          {name: 'Mandarina', value: 1400, quantity: 10},
-          {name: 'Corozo', value: 1350, quantity: 10},
+          {name: 'Mandarina', value: 1400, quantity: 23},
+          {name: 'Corozo', value: 1350, quantity: 5},
       ],
       newItemName: '',
       newItemValue: 0,
-      newItemQuantity: 0
+      newItemQuantity: 0,
+      total: 0
     },
     methods: {
         agregarItem (){
             this.items.push(
                 {name: this.newItemName, value: this.newItemValue, quantity: this.newItemQuantity}
-            )
+            );
+            this.newItemName =  '';
+            this.newItemQuantity = 0;
+            this.newItemValue = 0;
+        }
+    },
+    computed: {
+        sumaTotal (){
+            this.total = 0;
+            for(item of this.items){
+                this.total += item.quantity;
+            }
+            return this.total;
         }
     },
   })
