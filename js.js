0c8e8deaf02ac3087fdb6c904a054708b20691b8
Jhordy Barrera <jbarreraballestas@gmail.com>
2020-06-21 15:53:21 -0500

    v

--- js.js
+++ js.js
@@ -1,6 +1,23 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
-    }
+      message: 'Hello Vue!',
+      items: [
+          {name: 'Mango', value: 1000, quantity: 10},
+          {name: 'Patilla', value: 5500, quantity: 10},
+          {name: 'Pera', value: 2000, quantity: 0},
+          {name: 'Mandarina', value: 1400, quantity: 10},
+          {name: 'Corozo', value: 1350, quantity: 10},
+      ],
+      newItemName: '',
+      newItemValue: 0,
+      newItemQuantity: 0
+    },
+    methods: {
+        agregarItem (){
+            this.items.push(
+                {name: this.newItemName, value: this.newItemValue, quantity: this.newItemQuantity}
+            )
+        }
+    },
   })
